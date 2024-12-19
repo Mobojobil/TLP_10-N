18f5643ed42c4be19400662a0ad3b2bb1631a4b8
ThomasShelbit <tomasdomingosg09@gmail.com>
2024-12-19 15:02:45 +0000

    Add files via upload

---
+++ Natal.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void main()
+{
+	string pessoas[22] = { "Clara", "Abdulah", "André", "Andrei", "Hugo", "Paloma", "João", "Leonardo", "Miguel", "Pedro", "Pierre", "Rafael", "Ricardo", "Salvador", "Sartaj", "Tiago", "Tomás Filipe", "Ulisses", "Henrise", "Heros", "Tomás", "Tuqeer" };
+	string produto[6] = { "Batatas Fritas", "Bolo Rei", "Perfume", "Par de Meias", "Pijama", "Chocolate com Ginja" };
+	int tabela[100][100], total = 0, totalP = 0, totalT = 0;
+	for (int i = 0; i < 22; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			cout << "quantas/os " << produto[j] << " a/o " << pessoas[i] << " trouxe para o natal ";
+			cin >> tabela[j][i];
+		}
+	}
+	for (int i = 0; i < 22; i++)
+	{
+		total = 0;
+		for (int j = 0; j < 6; j++)
+		{
+			total = total + tabela[j][i];
+		}
+		cout << "A pessoa " << pessoas[i] << " trouxe " << total << "\n";
+	}
+	for (int i = 0; i < 22; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			totalP = totalP + tabela[j][i];
+		}
+	}
+	cout << "O total de produtos trazidos foram " << totalP << "\n";
+	for (int i = 0; i < 6; i++)
+	{
+		totalT = 0;
+		for (int j = 0; j < 22; j++)
+		{
+			totalT = totalT + tabela[i][j];
+		}
+		cout << "No total tem " << totalT << " " << produto[i] << "\n";
+	}
