9c4df8baee22f1beb5c1574477567a5e446b750e
ThomasShelbit <tomasdomingosg09@gmail.com>
2025-02-17 18:17:20 +0000

    Add files via upload

---
+++ pagar_a_mielas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace  std;
+double valores(float nrpessoas, float despesa) {
+	float resultado;
+	resultado = despesa / nrpessoas;
+	return resultado;}
+void main() {
+	float despesa, resultado, nrpessoas;
+	cin >> nrpessoas >> despesa;
+	resultado = valores(nrpessoas, despesa);
+	cout << resultado;}
+
